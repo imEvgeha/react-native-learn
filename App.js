323b636a000c0fa4740b0f1e961de5f9ada93de0
Evgeniy <oskarroker@gmail.com>
2021-01-06 12:17:55 +0200

    Code optimize

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { View } from 'react-native';
 import Home from './Home';
 import BuisnessPage from './BuisnessPage';
 import ToDoInfo from './ToDoInfo';
 import {NativeRouter, Switch, Route} from 'react-router-native'
-
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
@@ -41,13 +18,5 @@ const App: () => React$Node = () => {
     </NativeRouter>
   );
 };
-
-const styles = StyleSheet.create({
-  // greet: {
-  //   textAlign: 'center',
-  //   fontSize: 24,
-  //   fontWeight: '800',
-  // },
-});
 
 export default App;
